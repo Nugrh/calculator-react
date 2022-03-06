2976ee0b5f224ba91a96cbd1cebad34e7b0a23f0
Anugrah Tribima <73867671+Nugrh@users.noreply.github.com>
2022-03-06 22:18:04 +0700

    Send data to parrent component

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,31 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Button.css";
 
-const Button = () => {
+const Button = (props) => {
+    const [inputNum, setInputNum] = useState({
+        num: 0,
+        operator: "",
+        result: 0,
+    });
+
+    props.onSubmitNum(inputNum);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const handleInput = (e) => {
+        setInputNum({ num: e.target.value });
+    };
+
+    const handleOperator = (e) => {
+        setInputNum({
+            operator: e.target.value,
+        });
+    };
+
     return (
-        <form className="grid-container">
-            {/* first row */}
-            <input className="grid-item" type={"button"} value={"%"} />
-            <input className="grid-item" type={"button"} value={"X"} />
-            <input className="grid-item" type={"button"} value={"+"} />
+        <div>
+            <form onSubmit={handleOperator} className="grid-container">
+                {/* first row */}
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={"%"}
+                    operator={"%"}
+                    onClick={(e) => handleOperator(e)}
+                />
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={"X"}
+                    operator={"X"}
+                    onClick={(e) => handleOperator(e)}
+                />
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={"+"}
+                    operator={"+"}
+                    onClick={(e) => handleOperator(e)}
+                />
+            </form>
+            <form onSubmit={handleSubmit} className="grid-container">
+                {/* second row */}
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={7}
+                    onClick={(e) => handleInput(e)}
+                />
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={8}
+                    onClick={(e) => handleInput(e)}
+                />
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={9}
+                    onClick={(e) => handleInput(e)}
+                />
 
-            {/* second row */}
-            <input className="grid-item" type={"button"} value={"7"} />
-            <input className="grid-item" type={"button"} value={"8"} />
-            <input className="grid-item" type={"button"} value={"9"} />
+                {/* third row */}
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={4}
+                    onClick={(e) => handleInput(e)}
+                />
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={5}
+                    onClick={(e) => handleInput(e)}
+                />
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={6}
+                    onClick={(e) => handleInput(e)}
+                />
 
-            {/* third row */}
-            <input className="grid-item" type={"button"} value={"4"} />
-            <input className="grid-item" type={"button"} value={"5"} />
-            <input className="grid-item" type={"button"} value={"6"} />
-
-            {/* fourth row */}
-            <input className="grid-item" type={"button"} value={"<-"} />
-            <input className="grid-item" type={"button"} value={"0"} />
-            <button className="grid-item" type={"submit"}>
-                =
-            </button>
-        </form>
+                {/* fourth row */}
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={"⇐"}
+                    onClick={(e) => handleInput(e)}
+                />
+                <input
+                    className="grid-item"
+                    type={"button"}
+                    value={0}
+                    onClick={(e) => handleInput(e)}
+                />
+                <button className="grid-item" type={"submit"}>
+                    =
+                </button>
+            </form>
+        </div>
     );
 };
 
--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../button/Button";
 import "./Calculator.css";
 
 const Calculator = () => {
+    const [submitNum, setSubmitNum] = useState({
+        num: 0,
+        operator: "",
+        result: 0,
+    });
+
+    const onSubmitNum = (value) => {
+        setSubmitNum(value);
+    };
+
     return (
         <div className="border">
             <h2>Calculator App</h2>
-            <input className="result-input" disabled value={0} />
-            <Button />
+            <input className="result-input" disabled value={submitNum.num} />
+            <Button onSubmitNum={onSubmitNum} />
         </div>
     );
 };
